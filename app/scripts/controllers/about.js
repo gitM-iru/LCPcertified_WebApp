c1902f9b8716ab9efdfac539e2f68c651c8c74a5
gitM-iru <matnafub@gmail.com>
2015-03-03 23:14:51 -0800

    Initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name lcpcertifiedWebAppApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the lcpcertifiedWebAppApp
+ */
+angular.module('lcpcertifiedWebAppApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name lcpcertifiedWebAppApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the lcpcertifiedWebAppApp
+ */
+angular.module('lcpcertifiedWebAppApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
